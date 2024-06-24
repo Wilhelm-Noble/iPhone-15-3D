0a2782237143dc87520859a5d0a835d335f2d8dd
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-06-25 06:20:06 +0800

    Add files via upload

---
+++ action-images.js
@@ -0,0 +1,42 @@
+import silent  from '../assets/images/action/silent.png';
+import silentIcon from '../assets/images/action/silentIcon.png';
+import focus  from '../assets/images/action/focus.png';
+import focusIcon from '../assets/images/action/focusIcon.png';
+import camera  from '../assets/images/action/camera.png';
+import cameraIcon from '../assets/images/action/cameraIcon.png';
+import flash  from '../assets/images/action/flash.png';
+import flashIcon from '../assets/images/action/flashIcon.png';
+import voice  from '../assets/images/action/voice.png';
+import voiceIcon from '../assets/images/action/voiceIcon.png';
+import translate  from '../assets/images/action/translate.png';
+import translateIcon from '../assets/images/action/translateIcon.png';
+import zoom  from '../assets/images/action/zoom.png';
+import zoomIcon from '../assets/images/action/zoomIcon.png';
+import shortcut  from '../assets/images/action/shortcut.png';
+import shortcutIcon from '../assets/images/action/shortcutIcon.png';
+import access  from '../assets/images/action/access.png';
+import accessIcon from '../assets/images/action/accessIcon.png';
+
+export const actionsImg = [
+    { name: 'Silent Mode', image: silent, icon: silentIcon },
+    { name: 'Focus', image: focus, icon: focusIcon },
+    { name: 'Camera', image: camera, icon: cameraIcon },
+    { name: 'Flashlight', image: flash, icon: flashIcon },
+    { name: 'Voice Memo', image: voice, icon: voiceIcon },
+    { name: 'Translate', image: translate, icon: translateIcon },
+    { name: 'Magnifier', image: zoom, icon: zoomIcon },
+    { name: 'Shortcut', image: shortcut, icon: shortcutIcon },
+    { name: 'Accessibility', image: access, icon: accessIcon }
+];
+
+// export const actionsImg = [
+//     { name: 'Silent Mode', image: silent, icon: silentIcon },
+//     { name: 'Focus', image: focus, icon: focusIcon },
+//     { name: 'Camera', image: camera, icon: cameraIcon },
+//     { name: 'Flashlight', image: flash, icon: flashIcon },
+//     { name: 'Voice Memo', image: voice, icon: voiceIcon },
+//     { name: 'Translate', image: translate, icon: translateIcon },
+//     { name: 'Magnifier', image: zoom, icon: zoomIcon },
+//     { name: 'Shortcut', image: shortcut, icon: shortcutIcon },
+//     { name: 'Accessibility', image: access, icon: accessIcon }
+// ];
